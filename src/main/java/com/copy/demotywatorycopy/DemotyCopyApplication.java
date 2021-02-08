6bf57516b8ec0598d5edaa5ae21b2798ef113428
patryk <patryk.marcisz.v2@gmail.com>
2021-02-08 01:57:44 +0100

    initial commit

---
+++ src/main/java/com/copy/demotywatorycopy/DemotyCopyApplication.java
@@ -0,0 +1,13 @@
+package com.copy.demotywatorycopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotyCopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotyCopyApplication.class, args);
+    }
+
+}
